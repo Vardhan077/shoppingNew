a176dcb964c264178f325d5700d94b71d2cfcdd5
Vardhan077 <132035215+Vardhan077@users.noreply.github.com>
2024-06-13 21:57:57 +0530

    finalCommit

--- shopping/src/components/cart.js
+++ shopping/src/components/cart.js
@@ -8,6 +8,7 @@ import '../styles/products.css';
 import { useNavigate } from "react-router-dom";
 import Cookies from 'js-cookie'
 
+import PacmanLoader from "react-spinners/PacmanLoader";
 
 
 
@@ -17,11 +18,14 @@ export default function Cart() {
   const [auth, setAuth] = useState(false);
   const [cart, setCart] = useState([]);
   const [email, setEmail] = useState();
-  const [id,setId] = useState();
-  const [conf,setConf] = useState(false);
+  const [id, setId] = useState();
+  const [conf, setConf] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    axios.post("https://spectre-backend.onrender.com",{"token":Cookies.get('token')}).then((res) => {
+    setLoading(true)
+
+    axios.get("https://spectre-backend.onrender.com", { "token": Cookies.get('token') }).then((res) => {
       console.log(res.data)
       if (res.data.Status === "Success") {
         setEmail(res.data.email);
@@ -29,18 +33,22 @@ export default function Cart() {
       } else {
         setAuth(false);
       }
+    }).catch((err) => {
+      console.log(err);
+    }).finally(() => {
+      setLoading(false)
     });
-  }, []); // Empty dependency array to run only once when the component mounts
+  }, []);
 
   useEffect(() => {
-    console.log(email,"check")
+    console.log(email, "check")
     if (auth && email) {
       axios
         .get(`https://spectre-backend.onrender.com/retrieve?email=${email}`)
         .then((res) => {
           const data = JSON.parse(res.data);
           setCart(data);
-          
+
         })
         .catch((err) => {
           console.log(err);
@@ -53,31 +61,31 @@ export default function Cart() {
     console.log("Cart Data:", cart);
   }, [auth, cart]); // Log auth status and cart data
 
-  useEffect(()=>{
+  useEffect(() => {
     console.log(id)
-    axios.post("https://spectre-backend.onrender.com/removeitem",[id])
-    .then(res=>{
+    axios.post("https://spectre-backend.onrender.com/removeitem", [id])
+      .then(res => {
         window.location.reload()
         console.log(res)
-    })
-    .catch((err)=>{
+      })
+      .catch((err) => {
         console.log(err)
-    })
+      })
 
-  },[id])
+  }, [id])
 
-  const handleCheckout = ()=>{
+  const handleCheckout = () => {
     setConf(true)
     navigate("/checkout")
-    
+
   }
-  console.log(auth,"dhl");
+  console.log(auth, "dhl");
   if (!auth) {
     return (
       <div>
         <Navbar />
         <div className="flex flex-col justify-center items-center mt-32">
-        <p className="text-4xl font-bold">Bag</p>
+          <p className="text-4xl font-bold">Bag</p>
           <div className="pro flex flex-col justify-center items-center">
             <p>Welcome,</p>
             <p>Please login to continue</p>
@@ -95,29 +103,39 @@ export default function Cart() {
       return total + itemsArray.reduce((itemTotal, singleItem) => itemTotal + singleItem.price, 0);
     }, 0);
   };
-  const handleRemove = (id)=>{
+  const handleRemove = (id) => {
     setId(id)
   }
- 
+
   return (
     <div>
       <Navbar />
-      <div className="new">
-        {cart.map((item, index) => (
-          <div key={index}>
-            <CartCards item={item} handleRemove={handleRemove}/>
-          </div>
-        ))}
-      </div>
-      <div className="total-price text-center font-extrabold text-2xl text-green-700 mt-32">
-        <p>Total Price: ${calculateTotalPrice()}</p>
-      </div>
-      <div className="flex items-center justify-center mt-5">
-        <button className="butt mb-32" onClick={handleCheckout}>CheckOut!</button>
-      </div>
+      <div className= {loading ? "loadingContainer" : "contentContainer"}>
+        <div className="new">
+          {
+            loading ? (<PacmanLoader color="#ff0a54" />) : (
+              cart.map((item, index) => (
+                <div key={index}>
+                  <CartCards item={item} handleRemove={handleRemove} />
+                </div>
+              ))
+            )
 
-      <div>
-        <Footer />
+            
+          }
+        </div>
+        <div className="total-price text-center font-extrabold text-2xl text-green-700 mt-32">
+          <p>Total Price: ${calculateTotalPrice()}</p>
+        </div>
+        <div className="flex items-center justify-center mt-5">
+          <button className="butt mb-32" onClick={handleCheckout}>CheckOut!</button>
+        </div>
+
+        <div>
+          <Footer />
+        </div>
+
+
       </div>
     </div>
   );
